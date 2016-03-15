7c2ac1b4271c633ba14d7a8bd016917425890d53
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-03-15 13:10:10 +0000

    Fixed pyflakes import errors

--- holocube/__init__.py
+++ holocube/__init__.py
@@ -1,4 +1,4 @@
-from .element import (GeoElement, Cube, GeoFeature, GeoTiles,
-                      WMTS, Contours, Image, Points)  # noqa (API import)
-from . import plotting                                # noqa (API import)
-
+from .element import (GeoElement, Cube, GeoFeature, # noqa (API import)
+                      GeoTiles, WMTS, Contours,
+                      Image, Points)
+from . import plotting                              # noqa (API import)
--- holocube/element/__init__.py
+++ holocube/element/__init__.py
@@ -1,3 +1,4 @@
 from .cube import Cube                              # noqa (API import)
-from .geo import (GeoElement, GeoFeature, GeoTiles,
-                  WMTS, Points, Image, Contours)    # noqa (API import)
+from .geo import (GeoElement, GeoFeature, GeoTiles, # noqa (API import)
+                  WMTS, Points, Image, Contours)
+

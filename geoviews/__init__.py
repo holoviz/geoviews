0f1c129bb64c25f3bb5eb56c5e91a40ebc729b64
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-08-05 16:06:18 +0100

    Added function to copy notebooks to specified path

--- geoviews/__init__.py
+++ geoviews/__init__.py
@@ -1,4 +1,26 @@
+import os, glob
+from shutil import copyfile
+
 from .element import (_Element, Feature, Tiles,     # noqa (API import)
                       WMTS, LineContours, FilledContours, Text, Image,
                       Points, Path, Polygons, Shape)
 from . import plotting                              # noqa (API import)
+
+
+def example_notebooks(path='.', verbose=False):
+    """
+    Copies the example Jupyter notebooks to the supplied path.
+    """
+    path = os.path.abspath(path)
+    if not os.path.exists(path):
+        os.makedirs(path)
+        if verbose:
+            print('Created directory %s' % path)
+    notebook_glob = os.path.join(__path__[0], '..', 'doc', '*.ipynb')
+    notebooks = glob.glob(notebook_glob)
+    for notebook in notebooks:
+        nb_path = os.path.join(path, os.path.basename(notebook))
+        copyfile(notebook, nb_path)
+        if verbose:
+            print("%s copied to %s" % (os.path.basename(notebook), path))
+

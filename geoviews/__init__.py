81ae39062983fdc9e3d7c345bc56964e5ae5e6c3
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-08-17 15:47:08 +0100

    Added GeoViews Dataset class and fixed inheritance

--- geoviews/__init__.py
+++ geoviews/__init__.py
@@ -4,7 +4,7 @@ import param
 
 from .element import (_Element, Feature, Tiles,     # noqa (API import)
                       WMTS, LineContours, FilledContours, Text, Image,
-                      Points, Path, Polygons, Shape)
+                      Points, Path, Polygons, Shape, Dataset)
 from . import plotting                              # noqa (API import)
 
 

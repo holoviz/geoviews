ce9c1abe0ffb7d11711a2ff17f4c7f02338d8393
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-08-19 12:01:39 +0100

    Added function to download sample datasets

--- geoviews/__init__.py
+++ geoviews/__init__.py
@@ -1,5 +1,9 @@
 import os, glob
+import requests
+from io import BytesIO
 from shutil import copyfile
+from zipfile import ZipFile
+
 import param
 
 from .element import (_Element, Feature, Tiles,     # noqa (API import)
@@ -11,10 +15,13 @@ from . import plotting                              # noqa (API import)
 __version__ = param.Version(release=(1,0,0), fpath=__file__,
                             commit="$Format:%h$", reponame='geoviews')
 
+SAMPLE_DATA_URL = 'http://assets.holoviews.org/geoviews-sample-data.zip'
 
-def examples(path='geoviews-examples', verbose=False):
+
+def examples(path='geoviews-examples', include_data=False, verbose=False):
     """
-    Copies the examples to the supplied path.
+    Copies the example notebooks to the supplied path. If
+    include_data is enabled the sample data is also downloaded.
     """
 
     import os, glob
@@ -38,3 +45,30 @@ def examples(path='geoviews-examples', verbose=False):
                 if verbose:
                     print("%s copied to %s" % (nb_name, path))
             break
+
+    if include_data:
+        data_path = os.path.join(path, 'sample-data')
+        sample_data(data_path, verbose)
+
+
+def sample_data(path='../doc/sample-data', verbose=False):
+    """
+    Downloads and unzips the sample data to a particular location.
+    """
+    url = requests.get(SAMPLE_DATA_URL)
+    zipfile = ZipFile(BytesIO(url.content))
+    zip_names = zipfile.namelist()
+
+    path = os.path.abspath(path)
+    if not os.path.exists(path):
+        os.makedirs(path)
+        if verbose:
+            print('Created directory %s' % path)
+
+    for nc_file in zip_names:
+        extracted_file = zipfile.open(nc_file).read()
+        save_path = os.path.join(path, nc_file)
+        with open(save_path, 'wb') as f:
+            f.write(extracted_file)
+        if verbose:
+            print("%s downloaded to %s" % (nc_file, path))

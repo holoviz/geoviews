7092e40f0f98b8d8223bf04c7e0ff43e795216f4
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-08-19 11:35:22 +0100

    Updated function to fetch examples

--- geoviews/__init__.py
+++ geoviews/__init__.py
@@ -11,20 +11,30 @@ from . import plotting                              # noqa (API import)
 __version__ = param.Version(release=(1,0,0), fpath=__file__,
                             commit="$Format:%h$", reponame='geoviews')
 
-def examples(path='.', verbose=False):
+
+def examples(path='geoviews-examples', verbose=False):
     """
-    Copies the example Jupyter notebooks to the supplied path.
+    Copies the examples to the supplied path.
     """
+
+    import os, glob
+    from shutil import copytree, ignore_patterns
+
+    candidates = [os.path.join(__path__[0], '../doc/'),
+                  os.path.join(__path__[0], '../../../../share/geoviews-examples')]
+
     path = os.path.abspath(path)
     if not os.path.exists(path):
         os.makedirs(path)
         if verbose:
             print('Created directory %s' % path)
-    notebook_glob = os.path.join(__path__[0], '..', 'doc', '*.ipynb')
-    notebooks = glob.glob(notebook_glob)
-    for notebook in notebooks:
-        nb_path = os.path.join(path, os.path.basename(notebook))
-        copyfile(notebook, nb_path)
-        if verbose:
-            print("%s copied to %s" % (os.path.basename(notebook), path))
 
+    for source in candidates:
+        if os.path.exists(source):
+            for nb in glob.glob(os.path.join(source, '*.ipynb')):
+                nb_name = os.path.basename(nb)
+                nb_path = os.path.join(path, nb_name)
+                copyfile(nb, nb_path)
+                if verbose:
+                    print("%s copied to %s" % (nb_name, path))
+            break

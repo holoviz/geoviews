52380229c22d362cb04ae3e9d72448ab68bc31b9
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-06-29 19:18:35 +0100

    Added geoviews RGB type

--- geoviews/__init__.py
+++ geoviews/__init__.py
@@ -8,7 +8,7 @@ import param
 
 from .element import (_Element, Feature, Tiles,     # noqa (API import)
                       WMTS, LineContours, FilledContours, Text, Image,
-                      Points, Path, Polygons, Shape, Dataset)
+                      Points, Path, Polygons, Shape, Dataset, RGB)
 from . import operation                             # noqa (API import)
 from . import plotting                              # noqa (API import)
 from . import feature                               # noqa (API import)

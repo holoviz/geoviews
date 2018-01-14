1121ece26d6092f507ea9fcc89139a280eae4a15
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-01-14 18:50:31 +0000

    Fixed updating of matplotlib WMTS

--- geoviews/plotting/mpl/__init__.py
+++ geoviews/plotting/mpl/__init__.py
@@ -449,8 +449,20 @@ class WMTSPlot(GeoPlot):
 
     def init_artists(self, ax, plot_args, plot_kwargs):
         if isinstance(plot_args[0], GoogleTiles):
-            return {'artist': ax.add_image(*plot_args, **plot_kwargs)}
+            if 'artist' in self.handles:
+                return {'artist': self.handles['artist']}
+            img = ax.add_image(*plot_args, **plot_kwargs)
+            return {'artist': img or plot_args[0]}
         return {'artist': ax.add_wmts(*plot_args, **plot_kwargs)}
+
+    def teardown_handles(self):
+        """
+        If no custom update_handles method is supplied this method
+        is called to tear down any previous handles before replacing
+        them.
+        """
+        if not isinstance(self.handles.get('artist'), GoogleTiles):
+            self.handles['artist'].remove()
 
 
 

d192dee25b3cfbcef6aea9c04b28aa58d047e90f
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-02-12 04:00:08 +0000

    Bumped version to 1.4.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras_require['notebook-dependencies'] = ['jupyter', 'pyzmq', 'jinja2', 'tornad
 
 setup_args.update(dict(
     name='geoviews',
-    version="1.4.2",
+    version="1.4.3",
     install_requires = install_requires,
     extras_require = extras_require,
     description='GeoViews.',

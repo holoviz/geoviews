939b5f2fc3df1d15ea532f486b1576c1cce1e2d5
jlstevens <jlstevens@ed.ac.uk>
2018-03-12 17:55:45 +0000

    Updated build.sh to copy notebooks to correct path

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -4,6 +4,6 @@ SRC_DIR=$RECIPE_DIR/..
 pushd $SRC_DIR
 
 $PYTHON setup.py --quiet install --single-version-externally-managed --record=record.txt
-cp -r $SRC_DIR/doc/ $PREFIX/share/geoviews-examples/
 
+cp -r $SRC_DIR/notebooks/ $PREFIX/share/geoviews-examples/
 popd

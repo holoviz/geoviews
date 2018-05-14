d7c0c836a3102acb03f839359183802e5400cda2
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-05-15 00:59:38 +0100

    Further fix to projection inference

--- geoviews/plotting/plot.py
+++ geoviews/plotting/plot.py
@@ -31,7 +31,7 @@ class ProjectionPlot(param.Parameterized):
                                       keyfn=isoverlay, defaults=False)
         from_overlay = not all(p is None for p in opts[True]['projection'])
         projections = opts[from_overlay]['projection']
-        infer = any(opts[from_overlay]['infer_projection'])
+        infer = any(opts[from_overlay]['infer_projection']) or self.infer_projection
         custom_projs = [p for p in projections if p is not None]
 
         if len(set([type(p) for p in custom_projs])) > 1:

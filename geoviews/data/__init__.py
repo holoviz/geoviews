5bc83b4b73612c604dff44118b59878c1e90a858
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-10-08 18:10:52 +0100

    Added iris interface

--- geoviews/data/__init__.py
+++ geoviews/data/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import param
+from holoviews.core.data import Dataset
 
 try:
     from . import geopandas # noqa (API import)
@@ -8,5 +9,14 @@ except ImportError:
     pass
 except Exception as e:
     param.main.warning('GeoPandas interface failed to import with '
+                       'following error: %s' % e)
+
+try:
+    from . import iris # noqa (API import)
+    Dataset.datatype.append('cube')
+except ImportError:
+    pass
+except Exception as e:
+    param.main.warning('Iris interface failed to import with '
                        'following error: %s' % e)
 

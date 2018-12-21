10193aaa74879e3e275babdaeeb3f9ad1987a0db
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-12-21 17:25:15 +0000

    Losened cartopy version requirement

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ def package_assets(example_path):
 
 _required = [
     'bokeh >=0.12.15',
-    'cartopy >=0.17.0',  # prevents pip alone (requires external package manager)
+    'cartopy >=0.16.0',  # prevents pip alone (requires external package manager)
     'holoviews >=1.11.0a12',
     'numpy >=1.0',
     'param >=1.6.1'

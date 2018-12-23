1b58042bf7f62381a180a16928a13bdeccd74182
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-12-23 15:11:28 +0000

    Further fix for array conversion

--- geoviews/util.py
+++ geoviews/util.py
@@ -297,8 +297,19 @@ def to_ccw(geom):
 
 
 def geom_to_arr(geom):
-    data = geom.array_interface()
-    return np.array(data['data']).reshape(data['shape'])[:, :2]
+    try:
+        xy = getattr(geom, 'xy', None)
+    except NotImplementedError:
+        xy = None
+    if xy is not None:
+        return np.column_stack(xy)
+    if hasattr(geom, 'array_interface'):
+        data = geom.array_interface()
+        return np.array(data['data']).reshape(data['shape'])[:, :2]
+    arr = geom.array_interface_base['data']
+    if (len(arr) % 2) != 0:
+        arr = arr[:-1]
+    return np.array(arr).reshape(int(len(arr)/2), 2)
 
 
 def geom_length(geom):

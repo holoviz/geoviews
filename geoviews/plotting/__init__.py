f99d74dd8a1944d9bd9382e83091b900f4f76214
Philipp Rudiger <prudiger@anaconda.com>
2019-01-25 18:46:02 +0000

    Added plotting backend load hooks

--- geoviews/plotting/__init__.py
+++ geoviews/plotting/__init__.py
@@ -1,13 +1,34 @@
+from holoviews import Store, extension
 from holoviews.core.options import Compositor
 from holoviews.operation.element import contours
 
 from ..element import Contours, Polygons
-from . import mpl # noqa
 
-try:
-    from . import bokeh # noqa
-except ImportError:
-    pass
+
+if hasattr(extension, 'register_backend_callback'):
+    def _load_bokeh():
+        from . import bokeh # noqa
+    extension.register_backend_callback('bokeh', _load_bokeh)
+
+    def _load_mpl():
+        from . import mpl # noqa
+    extension.register_backend_callback('matplotlib', _load_mpl)
+
+    backends = Store.loaded_backends()
+    if 'bokeh' in backends:
+        _load_bokeh()
+    if 'matplotlib' in backends:
+        _load_mpl()
+else:
+    try:
+        from . import mpl # noqa
+    except ImportError:
+        pass
+
+    try:
+        from . import bokeh # noqa
+    except ImportError:
+        pass
 
 Compositor.register(Compositor("LineContours", contours, None,
                                'data', transfer_options=True,

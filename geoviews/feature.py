2009926d6af4987a24b7d360dd9fc1df60f78220
ahuang11 <ahuang11@illinois.edu>
2019-04-13 15:57:23 -0700

    Woops caps lock was on

--- geoviews/feature.py
+++ geoviews/feature.py
@@ -8,7 +8,7 @@ land      = Feature(cf.LAND, group='Land')
 lakes     = Feature(cf.LAKES, group='Lakes')
 ocean     = Feature(cf.OCEAN, group='Ocean')
 rivers    = Feature(cf.RIVERS, group='Rivers')
-states    = FEATURE(cf.STATES, group='States')
+states    = Feature(cf.STATES, group='States')
 grid      = Feature(cf.NaturalEarthFeature(category='physical',
                                            name='graticules_30',
                                            scale='110m'), group='Grid')

cc70ac2d5a96307769bc6192eaef8576c3d24b30
Andrew Huang <huang.andrew12@gmail.com>
2019-04-14 15:20:10 -0700

    States features (#312)
    * Fix gridlines for gv matplotlib overlay
    
    * Add states to features
    
    * Woops caps lock was on

--- geoviews/feature.py
+++ geoviews/feature.py
@@ -8,6 +8,7 @@ land      = Feature(cf.LAND, group='Land')
 lakes     = Feature(cf.LAKES, group='Lakes')
 ocean     = Feature(cf.OCEAN, group='Ocean')
 rivers    = Feature(cf.RIVERS, group='Rivers')
+states    = Feature(cf.STATES, group='States')
 grid      = Feature(cf.NaturalEarthFeature(category='physical',
                                            name='graticules_30',
                                            scale='110m'), group='Grid')

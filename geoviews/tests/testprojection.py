f8f1992ed4a0ddc08cd186fb6888a894292dc3de
Philipp Rudiger <prudiger@anaconda.com>
2019-10-11 18:55:11 +0200

    Fixed issues with target extents in project_image (#365)

--- geoviews/tests/testprojection.py
+++ geoviews/tests/testprojection.py
@@ -9,29 +9,25 @@ from geoviews.operation import project
 class TestProjection(ComparisonTestCase):
 
     def test_image_latlon360_wrapping(self):
-        xs = np.linspace(0, 360, 5)
-        ys = np.linspace(-90, 90, 3)
+        xs = np.linspace(72, 360, 5)
+        ys = np.linspace(-60, 60, 3)
         img = Image((xs, ys, xs[np.newaxis, :]*ys[:, np.newaxis]))
         proj = project(img, projection=ccrs.PlateCarree())
         zs = proj.dimension_values('z', flat=False)
         self.assertEqual(zs, np.array([
-            [-24300.,     -0.,  -8100.],
-            [     0.,      0.,      0.],
-            [     0.,      0.,      0.],
-            [     0.,      0.,      0.],
-            [ 24300.,      0.,   8100.]
+            [-12960., -17280., -21600.,  -4320.,  -8640.],
+            [     0.,      0.,      0.,      0.,      0.],
+            [ 12960.,  17280.,  21600.,   4320.,   8640.]
         ]))
 
     def test_image_project_latlon_to_mercator(self):
-        xs = np.linspace(0, 360, 5)
-        ys = np.linspace(-90, 90, 3)
+        xs = np.linspace(72, 360, 5)
+        ys = np.linspace(-60, 60, 3)
         img = Image((xs, ys, xs[np.newaxis, :]*ys[:, np.newaxis]))
         proj = project(img)
         zs = proj.dimension_values('z', flat=False)
         self.assertEqual(zs, np.array([
-            [-24300.,     -0.,  -8100.],
-            [-24300.,     -0.,  -8100.],
-            [     0.,      0.,      0.],
-            [ 24300.,      0.,   8100.],
-            [ 24300.,      0.,   8100.]
+            [-12960., -17280., -21600.,  -4320.,  -8640.],
+            [     0.,      0.,      0.,      0.,      0.],
+            [ 12960.,  17280.,  21600.,   4320.,   8640.]
         ]))

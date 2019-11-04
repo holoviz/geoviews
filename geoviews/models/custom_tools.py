6b8890c38a676861afb4800384ef8c809cd15e95
Philipp Rudiger <prudiger@anaconda.com>
2019-11-04 10:27:18 -0600

    Fixed flakes

--- geoviews/models/custom_tools.py
+++ geoviews/models/custom_tools.py
@@ -1,7 +1,4 @@
-import os
-
 from bokeh.core.properties import Instance, List, Dict, String, Any
-from bokeh.core.enums import Dimensions
 from bokeh.models import Tool, ColumnDataSource, PolyEditTool, PolyDrawTool
 
 

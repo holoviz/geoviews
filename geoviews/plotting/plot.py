dfe469585a349e22182b222b33e74e27e9ef2640
Philipp Rudiger <prudiger@anaconda.com>
2020-01-15 23:33:14 +0100

    Compatibility for changes in HoloViews

--- geoviews/plotting/plot.py
+++ geoviews/plotting/plot.py
@@ -33,9 +33,9 @@ class ProjectionPlot(param.Parameterized):
         opts = self._traverse_options(obj, 'plot', ['projection', 'infer_projection'],
                                       [CompositeOverlay, Element],
                                       keyfn=isoverlay, defaults=False)
-        from_overlay = not all(p is None for p in opts[True]['projection'])
-        projections = opts[from_overlay]['projection']
-        infer = any(opts[from_overlay]['infer_projection']) or self.infer_projection
+        from_overlay = not all(p is None for p in opts.get(True, {}).get('projection', []))
+        projections = opts.get(from_overlay, {}).get('projection', [])
+        infer = any(opts.get(from_overlay, {}).get('infer_projection', [])) or self.infer_projection
         custom_projs = [p for p in projections if p is not None]
 
         if len(set([type(p) for p in custom_projs])) > 1:

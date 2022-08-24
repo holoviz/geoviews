060c71741e429d6e675976f3e906215354fbf87c
Maxime Liquet <35924738+maximlt@users.noreply.github.com>
2022-08-24 08:50:20 +0200

    Fix the docs build (#588)

--- setup.py
+++ setup.py
@@ -130,13 +130,14 @@ extras_require={
     'recommended': _recommended,
     'examples_extra': _examples_extra,
     'doc': _examples_extra + [
-        'nbsite >=0.7.1',
+        'nbsite >=0.7.2rc10',
         'cartopy >=0.20.0',
         'graphviz',
         'lxml',
         'selenium',
         'pooch',
         'pydata-sphinx-theme <0.9.0',
+        'sphinx-copybutton',
     ],
     'tests': [
         'pytest-cov',

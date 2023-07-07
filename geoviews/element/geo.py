c18e748bc82c081e0ddd4cc378518388222d73cd
Andrew Huang <ahuang@anaconda.com>
2023-07-07 13:30:03 -0400

    Support variety of data inputs

--- geoviews/element/geo.py
+++ geoviews/element/geo.py
@@ -353,29 +353,30 @@ class WindBarbs(_Element, Selection2DExpr, HvGeometry):
 
     @classmethod
     def from_uv(cls, data, kdims=None, vdims=None, **params):
-        if isinstance(data, tuple):
-            xs, ys, us, vs = data
-        else:
-            us = data[vdims[0]]
-            vs = data[vdims[1]]
+        if kdims is None:
+            kdims = ['x', 'y']
+        if vdims is None:
+            vdims = ['u', 'v']
+        dataset = Dataset(data, kdims=kdims, vdims=vdims, **params)
+        us, vs = (dataset.dimension_values(i) for i in range(2, 4))
 
         uv_magnitudes = np.hypot(us, vs)  # unscaled
         radians = np.pi / 2 - np.arctan2(-vs, -us)
 
-        if isinstance(data, tuple):
-            reorganized_data = (xs, ys, radians, uv_magnitudes)
-        else:
-            # calculations on this data could mutate the original data
-            # here we do not do any calculations; we only store the data
-            reorganized_data = {}
-            for kdim in kdims:
-                reorganized_data[kdim] = data[kdim]
-            reorganized_data["Angle"] = radians
-            reorganized_data["Magnitude"] = uv_magnitudes
-            for vdim in vdims[2:]:
-                reorganized_data[vdim] = data[vdim]
-            vdims = ["Angle", "Magnitude"] + vdims[2:]
-        return cls(reorganized_data, kdims=kdims, vdims=vdims, **params)
+        # calculations on this data could mutate the original data
+        # here we do not do any calculations; we only store the data
+        repackaged_dataset = {}
+        for kdim in kdims:
+            repackaged_dataset[kdim] = dataset[kdim]
+        repackaged_dataset["Angle"] = radians
+        repackaged_dataset["Magnitude"] = uv_magnitudes
+        for vdim in vdims[2:]:
+            repackaged_dataset[vdim] = dataset[vdim]
+        vdims = [
+            Dimension('Angle', cyclic=True, range=(0, 2 * np.pi)),
+            Dimension('Magnitude')
+        ] + vdims[2:]
+        return cls(repackaged_dataset, kdims=kdims, vdims=vdims, **params)
 
 
 class Image(_Element, HvImage):

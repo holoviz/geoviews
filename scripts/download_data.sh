654a9d141964490d7008b5d39692c6a6dbf38625
Simon Høxbro Hansen <simon.hansen@me.com>
2023-09-26 09:29:28 +0200

    Move download data into else

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -10,8 +10,9 @@ geoviews fetch-data --path="$HERE/../examples"
 python -c "
 try:
     import geodatasets as gds
-    gds.get_path('geoda airbnb')
-    gds.get_path('nybb')
 except ImportError:
     pass
+else:
+    gds.get_path('geoda airbnb')
+    gds.get_path('nybb')
 "

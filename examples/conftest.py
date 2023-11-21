f31941c80d50b7481d385286494725a7c3e642be
Simon Høxbro Hansen <simon.hansen@me.com>
2023-11-21 12:44:09 +0100

    Update pytest to align with HoloViews

---
+++ examples/conftest.py
@@ -0,0 +1,11 @@
+
+# depend on optional iris, xesmf, etc
+collect_ignore_glob = [
+    "Homepage.ipynb",
+    "user_guide/Resampling_Grids.ipynb",
+    "user_guide/Gridded_Datasets_*.ipynb",
+    "gallery/bokeh/xarray_gridded.ipynb",
+    "gallery/*/xarray_image.ipynb",
+    "gallery/*/xarray_quadmesh.ipynb",
+    "gallery/*/katrina_track.ipynb",
+]

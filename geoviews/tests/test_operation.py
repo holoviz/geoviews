633fa4e32819339e6d9520aa0fc4e35c683a8aae
Simon Høxbro Hansen <simon.hansen@me.com>
2024-01-23 11:48:28 +0100

    Maintenance of GeoViews (#696)

--- geoviews/tests/test_operation.py
+++ geoviews/tests/test_operation.py
@@ -1,7 +1,13 @@
-import geoviews as gv
+import pytest
 import xarray as xr
 from holoviews.operation import contours
 
+import geoviews as gv
+
+
+@pytest.mark.filterwarnings(
+    "ignore:numpy.ndarray size changed, may indicate binary incompatibility"  # https://github.com/pydata/xarray/issues/7259
+)
 def test_quadmesh_contoures_filled():
     # Regression test for: https://github.com/holoviz/holoviews/pull/5925
     ds = xr.tutorial.open_dataset("air_temperature").isel(time=0)

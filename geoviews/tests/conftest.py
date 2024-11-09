d28b034ba9bcc51e8884fc2eb197af555f3b731a
Andrew Huang <ahuang@anaconda.com>
2024-11-09 06:29:19 -0800

    suppress in pyproj

--- geoviews/tests/conftest.py
+++ geoviews/tests/conftest.py
@@ -1,6 +1,44 @@
 from contextlib import suppress
 
+from holoviews.tests.conftest import bokeh_backend, port, serve_hv  # noqa: F401
+
 import geoviews as gv
+
+CUSTOM_MARKS = ("ui",)
+
+
+def pytest_addoption(parser):
+    for marker in CUSTOM_MARKS:
+        parser.addoption(
+            f"--{marker}",
+            action="store_true",
+            default=False,
+            help=f"Run {marker} related tests",
+        )
+
+
+def pytest_configure(config):
+    for marker in CUSTOM_MARKS:
+        config.addinivalue_line("markers", f"{marker}: {marker} test marker")
+
+
+def pytest_collection_modifyitems(config, items):
+    skipped, selected = [], []
+    markers = [m for m in CUSTOM_MARKS if config.getoption(f"--{m}")]
+    empty = not markers
+    for item in items:
+        if empty and any(m in item.keywords for m in CUSTOM_MARKS):
+            skipped.append(item)
+        elif empty:
+            selected.append(item)
+        elif not empty and any(m in item.keywords for m in markers):
+            selected.append(item)
+        else:
+            skipped.append(item)
+
+    config.hook.pytest_deselected(items=skipped)
+    items[:] = selected
+
 
 with suppress(Exception):
     gv.extension("bokeh")
@@ -14,11 +52,8 @@ with suppress(ImportError):
     plt.switch_backend("agg")
 
 with suppress(Exception):
-    # From Dask 2023.7.1 they now automatically convert strings
-    # https://docs.dask.org/en/stable/changelog.html#v2023-7-1
     # From Dask 2024.3.0 they now use `dask_expr` by default
     # https://github.com/dask/dask/issues/10995
     import dask
 
-    dask.config.set({"dataframe.convert-string": False})
     dask.config.set({"dataframe.query-planning": False})

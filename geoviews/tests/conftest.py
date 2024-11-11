da8719e6775d01a0509c6e0c9597eb9301fc19a1
Andrew <15331990+ahuang11@users.noreply.github.com>
2024-11-10 23:37:34 -0800

    compat: Dask 2024.11 (#766)
    Co-authored-by: Simon Høxbro Hansen <simon.hansen@me.com>

--- geoviews/tests/conftest.py
+++ geoviews/tests/conftest.py
@@ -48,10 +48,3 @@ with suppress(ImportError):
     import matplotlib.pyplot as plt
 
     plt.switch_backend("agg")
-
-with suppress(Exception):
-    # From Dask 2024.3.0 they now use `dask_expr` by default
-    # https://github.com/dask/dask/issues/10995
-    import dask
-
-    dask.config.set({"dataframe.query-planning": False})

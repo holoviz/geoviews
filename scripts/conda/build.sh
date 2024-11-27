cafd5ec18f105b20bf0a93c3978108456d01a300
Simon Høxbro Hansen <simon.hansen@me.com>
2024-11-27 09:33:24 +0100

    Update channel list for conda build

--- scripts/conda/build.sh
+++ scripts/conda/build.sh
@@ -14,13 +14,13 @@ import bokeh
 from packaging.version import Version
 
 if Version(bokeh.__version__).is_prerelease:
-    print('bokeh/label/dev')
+    print('bokeh/label/rc')
 else:
     print('bokeh')
 ")
 
-conda build scripts/conda/recipe-core --no-anaconda-upload --no-verify -c conda-forge -c "$BK_CHANNEL" -c pyviz --package-format 1
-conda build scripts/conda/recipe-recommended --no-anaconda-upload --no-verify -c conda-forge -c "$BK_CHANNEL" -c pyviz --package-format 1
+conda build scripts/conda/recipe-core --no-anaconda-upload --no-verify -c pyviz -c "$BK_CHANNEL"  -c conda-forge --package-format 1
+conda build scripts/conda/recipe-recommended --no-anaconda-upload --no-verify -c pyviz -c "$BK_CHANNEL" -c conda-forge --package-format 1
 
 mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-core-$VERSION-py_0.tar.bz2" dist
 mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.tar.bz2" dist

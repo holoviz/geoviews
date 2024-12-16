20d301f652f85a11e9a59d335fb55648f2918c39
Simon Høxbro Hansen <simon.hansen@me.com>
2024-12-16 09:40:18 +0100

    compat: Remove Bokeh upper pinning

--- geoviews/models/custom_tools.py
+++ geoviews/models/custom_tools.py
@@ -1,8 +1,30 @@
+from functools import cache
+
+from bokeh import __version__ as bokeh_version
 from bokeh.core.properties import Any, Dict, Instance, List, String
 from bokeh.models import ColumnDataSource, PolyDrawTool, PolyEditTool, Tool
+from packaging.requirements import Requirement
+
+from .._warnings import warn
 
 
-class CheckpointTool(Tool):
+class _BokehCheck(Tool):
+    _bokeh_require_version = "bokeh ==3.6.*"
+    def __init__(self, *args, **kwargs):
+        self._check_bokeh_version()
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    @cache
+    def _check_bokeh_version(cls) -> None:
+        bokeh_req = Requirement(cls._bokeh_require_version)
+        if bokeh_req.specifier.contains(bokeh_version):
+            return
+        msg = f"{cls.__name__} only official supported with {cls._bokeh_require_version}, you have {bokeh_version} installed."
+        warn(msg, RuntimeWarning)
+
+
+class CheckpointTool(_BokehCheck, Tool):
     """
     Checkpoints the data on the supplied ColumnDataSources, allowing
     the RestoreTool to restore the data to a previous state.
@@ -11,7 +33,7 @@ class CheckpointTool(Tool):
     sources = List(Instance(ColumnDataSource))
 
 
-class RestoreTool(Tool):
+class RestoreTool(_BokehCheck, Tool):
     """
     Restores the data on the supplied ColumnDataSources to a previous
     checkpoint created by the CheckpointTool
@@ -20,7 +42,7 @@ class RestoreTool(Tool):
     sources = List(Instance(ColumnDataSource))
 
 
-class ClearTool(Tool):
+class ClearTool(_BokehCheck, Tool):
     """
     Clears the data on the supplied ColumnDataSources.
     """
@@ -28,7 +50,7 @@ class ClearTool(Tool):
     sources = List(Instance(ColumnDataSource))
 
 
-class PolyVertexEditTool(PolyEditTool):
+class PolyVertexEditTool(_BokehCheck, PolyEditTool):
 
     node_style = Dict(String, Any, help="""
     Custom styling to apply to the intermediate nodes of a patch or line glyph.""")
@@ -37,7 +59,7 @@ class PolyVertexEditTool(PolyEditTool):
     Custom styling to apply to the start and nodes of a patch or line glyph.""")
 
 
-class PolyVertexDrawTool(PolyDrawTool):
+class PolyVertexDrawTool(_BokehCheck, PolyDrawTool):
 
     node_style = Dict(String, Any, help="""
     Custom styling to apply to the intermediate nodes of a patch or line glyph.""")

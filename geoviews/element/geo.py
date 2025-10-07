785e2e21b8c761a142a5473b8dc91990a4876e4b
Andrew Huang <ahuang@anaconda.com>
2025-10-07 14:03:41 -0700

    add back

--- geoviews/element/geo.py
+++ geoviews/element/geo.py
@@ -417,6 +417,34 @@ class WindBarbs(_Element, Selection2DExpr, HvGeometry):
     vdims = param.List(default=[Dimension('Angle', cyclic=True, range=(0,2*np.pi)),
                                 Dimension('Magnitude')], bounds=(2, None))
 
+    @classmethod
+    def from_uv(cls, data, kdims=None, vdims=None, **params):
+        if kdims is None:
+            kdims = ['x', 'y']
+        if vdims is None:
+            vdims = ['u', 'v']
+        dataset = Dataset(data, kdims=kdims, vdims=vdims, **params)
+        us, vs = (dataset.dimension_values(i) for i in (2, 3))
+
+        uv_magnitudes = np.hypot(us, vs)  # unscaled
+        # using meteorological convention (direction FROM which wind blows)
+        radians = np.pi / 2 - np.arctan2(-vs, -us)
+
+        # calculations on this data could mutate the original data
+        # here we do not do any calculations; we only store the data
+        repackaged_dataset = {}
+        for kdim in kdims:
+            repackaged_dataset[kdim] = dataset[kdim]
+        repackaged_dataset["Angle"] = radians
+        repackaged_dataset["Magnitude"] = uv_magnitudes
+        for vdim in vdims[2:]:
+            repackaged_dataset[vdim] = dataset[vdim]
+        vdims = [
+            Dimension('Angle', cyclic=True, range=(0, 2 * np.pi)),
+            Dimension('Magnitude')
+        ] + vdims[2:]
+        return cls(repackaged_dataset, kdims=kdims, vdims=vdims, **params)
+
 
 class Image(_Element, HvImage):
     """Image represents a 2D array of some quantity with

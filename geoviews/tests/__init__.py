abdbcf01ac52b2c7b1218b70f6969f83ef02570b
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-12-15 16:40:16 +0100

    use better builtin

--- geoviews/tests/__init__.py
+++ geoviews/tests/__init__.py
@@ -1,4 +1,4 @@
-from holoviews.testing._testing import _ElementComparison as _EC
+from holoviews.testing import add_comparison
 
 from ..element.geo import (
     FilledContours,
@@ -9,11 +9,4 @@ from ..element.geo import (
     WindBarbs,
 )
 
-# TODO: Think more about this
-_EC.register()
-_EC.equality_funcs[Image] = _EC.compare_dataset
-_EC.equality_funcs[ImageStack] = _EC.compare_dataset
-_EC.equality_funcs[Points] = _EC.compare_dataset
-_EC.equality_funcs[LineContours] = _EC.compare_dataset
-_EC.equality_funcs[FilledContours] = _EC.compare_dataset
-_EC.equality_funcs[WindBarbs] = _EC.compare_dataset
+add_comparison(FilledContours, Image, ImageStack, LineContours, Points, WindBarbs)

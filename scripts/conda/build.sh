5f0de1b72272a0faa588d85f8733b7bdab48b163
Simon Høxbro Hansen <simon.hansen@me.com>
2026-09-14 09:38:59 +0200

    build: Work with Panel which supports Bokeh 3.10 (#877)

--- scripts/conda/build.sh
+++ scripts/conda/build.sh
@@ -19,8 +19,9 @@ else:
     print('bokeh')
 ")
 
-conda build scripts/conda/recipe-core --no-anaconda-upload --no-verify -c pyviz -c "$BK_CHANNEL" -c conda-forge --package-format 2
-conda build scripts/conda/recipe-recommended --no-anaconda-upload --no-verify -c pyviz -c "$BK_CHANNEL" -c conda-forge --package-format 2
+# TODO(build): Remove pyviz/label/dev after panel 1.10.0 release
+conda build scripts/conda/recipe-core --no-anaconda-upload --no-verify -c pyviz/label/dev -c "$BK_CHANNEL" -c conda-forge --package-format 2
+conda build scripts/conda/recipe-recommended --no-anaconda-upload --no-verify -c pyviz/label/dev -c "$BK_CHANNEL" -c conda-forge --package-format 2
 
 mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-core-$VERSION-py_0.conda" dist
 mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.conda" dist
